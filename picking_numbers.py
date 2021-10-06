05504df29247b1335090d57d074e9ec5d9b152b8
komega2409 <thienkhiemtran1999@gmail.com>
2021-10-06 16:41:23 +0700

    Finish picking_numbers.py

--- picking_numbers.py
+++ picking_numbers.py
@@ -16,18 +16,32 @@ import sys
 def pickingNumbers(a):
     # Write your code here
     a.sort()
+    a.append(0)
     subarr = []
     temp_arr = []
     int1 = 0
+    max_length = 0
 
     for i in range(len(a)):
         if not temp_arr:
             temp_arr.append(a[i])
-        
+            int1 = temp_arr[0]
+        else:
+            if a[i] - int1 == 0 or a[i] - int1 == 1:
+                temp_arr.append(a[i])
+            else:
+                subarr.append(temp_arr)
+                temp_arr = []
+                temp_arr.append(a[i])
+                int1 = temp_arr[0]
 
-
+    for sub in subarr:
+        if len(sub) > max_length:
+            max_length = len(sub)
+    return max_length
+    
 if __name__ == '__main__':
-    fptr = open('test_cases.txt', 'w')
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     n = int(input().strip())
 
