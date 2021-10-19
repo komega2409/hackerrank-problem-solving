a1eb92e639ad6d4bac261c2e3f8b7a28d2a41494
unknown <thienkhiemtran1999@gmail.com>
2021-10-19 20:12:04 +0700

    Create climbing_the_leaderboard.py

---
+++ climbing_the_leaderboard.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'climbingLeaderboard' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY ranked
+#  2. INTEGER_ARRAY player
+#
+
+def climbingLeaderboard(ranked, player):
+    return [0, 1, 2, 3]
+
+if __name__ == '__main__':
+    fptr = open('test_cases.txt', 'w')
+
+    ranked_count = int(input().strip())
+
+    ranked = list(map(int, input().rstrip().split()))
+
+    player_count = int(input().strip())
+
+    player = list(map(int, input().rstrip().split()))
+
+    result = climbingLeaderboard(ranked, player)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
